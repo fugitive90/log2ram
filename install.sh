0acffdaeb1e0b92b1e37db1d3825071149a49603
fugitive90 <mmilos311090@gmail.com>
2017-09-02 15:39:16 +0200

    Pre-check for update-rc

--- install.sh
+++ install.sh
@@ -4,13 +4,18 @@ if [ `id -u` -eq 0 ]
 then
   cp log2ram /etc/init.d/ 
   chmod 744 /etc/init.d/log2ram
-  update-rc.d log2ram defaults
-  cp log2ram.conf /etc/default/log2ram.conf
-  chmod 644 /etc/default/log2ram.conf
+  which update-rc.d >/dev/null
+  if [ $? -eq 0 ]; then
+	 update-rc.d log2ram defaults
+  else
+	 echo "Log2ram is not enabled on boot. Please update it with your init system."
+  fi
+
+  cp log2ram.conf /etc/log2ram.conf
+  chmod 644 /etc/log2ram.conf
   cp log2ram.hourly /etc/cron.hourly/log2ram
   chmod +x /etc/cron.hourly/log2ram
 
-  echo "##### Reboot to activate log2ram #####"
 else
   echo "You need to be ROOT (sudo can be used)"
 fi
